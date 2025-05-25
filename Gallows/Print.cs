28ec5c517f6ef6b15eb73a2943fdedf58e769c8e
Vitaly <157066065+HaIWaeR@users.noreply.github.com>
2025-05-25 23:27:19 +0300

    Add files via upload

---
+++ Gallows/Print.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace visilica
+{
+    internal class Print
+    {
+        public void HoverNumbers(int count)
+        {
+            Print loss = new Print();
+
+            switch (count)
+            {
+                case 1:
+                    loss.PositionBeginning();
+                    break;
+                case 2:
+                    loss.PositionHead();
+                    break;
+                case 3:
+                    loss.PositionTorso();
+                    break;
+                case 4:
+                    loss.PositionRightHand();
+                    break;
+                case 5:
+                    loss.PositionLeftHand();
+                    break;
+                case 6:
+                    loss.PositionRightFoot();
+                    break;
+                case 7:
+                    loss.PositionLeftFoot();
+                    break;
+            }
+        }
+
+        // 1
+        private void PositionBeginning()
+        {
+            string? line; 
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\Users\\vital\\source\\repos\\visilica\\visilica\\Hover\\Hover1.txt"))
+                {
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Исключение: " + e.Message);
+            }
+        }
+
+        // 2 
+        private void PositionHead()
+        {
+            string? line;
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\Users\\vital\\source\\repos\\visilica\\visilica\\Hover\\Hover2.txt"))
+                {
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Исключение: " + e.Message);
+            }
+        }
+
+        // 3
+        private void PositionTorso()
+        {
+            string? line;
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\Users\\vital\\source\\repos\\visilica\\visilica\\Hover\\Hover3.txt"))
+                {
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Исключение: " + e.Message);
+            }
+        }
+
+        // 4
+        private void PositionRightHand()
+        {
+            string? line;
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\Users\\vital\\source\\repos\\visilica\\visilica\\Hover\\Hover4.txt"))
+                {
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Исключение: " + e.Message);
+            }
+        }
+
+        // 5 
+        private void PositionLeftHand()
+        {
+            string? line;
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\Users\\vital\\source\\repos\\visilica\\visilica\\Hover\\Hover5.txt"))
+                {
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Исключение: " + e.Message);
+            }
+        }
+
+        // 6
+        private void PositionRightFoot()
+        {
+            string? line;
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\Users\\vital\\source\\repos\\visilica\\visilica\\Hover\\Hover6.txt"))
+                {
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Исключение: " + e.Message);
+            }
+        }
+        // 7 
+        private void PositionLeftFoot()
+        {
+            string? line;
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:\\Users\\vital\\source\\repos\\visilica\\visilica\\Hover\\Hover7.txt"))
+                {
+                    line = sr.ReadLine();
+                    while (line != null)
+                    {
+                        Console.WriteLine(line);
+                        line = sr.ReadLine();
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Исключение: " + e.Message);
+            }
+        }
+    }
+}
---
+++ Gallows/Program.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Common;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Runtime.InteropServices.ComTypes;
+using System.Security.AccessControl;
+using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+
+namespace visilica
+{
+    class MyClass
+    {
+        // Получаем слово 
+        public string GetWord(string answer)
+        {
+            string? word = null;
+
+            Console.Clear();
+
+            word = answer.ToLower();
+
+
+            if (!string.IsNullOrEmpty(word))
+            {
+                // Используем StringBuilder для формирования итоговой строки
+                var result = new System.Text.StringBuilder();
+
+                // Преобразуем StringBuilder в строку без пробелов и не букв
+                string finalResult = result.ToString();
+
+                // Преобразуем итоговую строку в верхний регистр
+                if (finalResult.Length > 0)
+                {
+                    word = finalResult.ToUpper();
+                }
+            }
+            return word;
+        }
+
+        public string GetLetter()
+        {
+            string? input;
+
+            // Запрашиваем у пользователя ввод буквы
+            while (true)
+            {
+                Console.Write("Введите букву: ");
+                //Console.Clear();
+                input = Console.ReadLine();
+
+                // Проверяем, что введено ровно 1 символ и он является буквой
+                if (!string.IsNullOrEmpty(input) && input.Length == 1 && char.IsLetter(input[0]))
+                    return input.ToLower();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var (answer, hint) = FileDate.InputData();
+
+                string tempHint = hint.ToUpper();
+
+                MyClass verification = new MyClass();
+
+                string word = answer.ToLower();
+
+                PrintWord textOutput = new PrintWord();
+
+                int count = 1;
+
+                textOutput.PrintCubeWord(word, count, tempHint);
+            }
+        }
+    }
+
+
+    class PrintWord
+    {
+        public void Hint(string tempHint)
+        {
+            Console.WriteLine();
+            Console.Write($" Подсказка: ");
+            ConsoleColor originalColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+
+            Console.WriteLine(tempHint.ToUpper());
+
+            Console.ForegroundColor = originalColor;
+            Console.WriteLine();
+        }
+
+
+        public void Design(string word)
+        {
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.Write(" - ");
+            }
+            Console.WriteLine();
+        }
+
+        public void PrintCubeWord(string word, int count, string tempHint)
+        {
+            Console.Clear();
+            
+
+            Print FirstImage = new Print();
+
+            char[] wordArray = new char[word.Length];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                wordArray[i] = word[i];
+            }
+
+            Design(word);
+
+            string[] charCube = new string[word.Length];
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                charCube[i] = "█";
+            }
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                Console.Write($" {charCube[i]} ");
+            }
+            Console.WriteLine();
+
+            Design(word);
+
+            Print printInstance = new Print();
+
+            while (count < 8)
+            {
+                Hint(tempHint);
+
+                printInstance.HoverNumbers(count);
+
+                MyClass verification = new MyClass();
+
+                string letter = verification.GetLetter();
+
+                List<int> indices = new List<int>();
+
+                char searchLetter = letter[0];
+
+                for (int i = 0; i < wordArray.Length; i++)
+                {
+                    if (wordArray[i] == searchLetter)
+                    {
+                        indices.Add(i);
+                    }
+                }
+
+                bool foundAny = false;
+                for (int i = 0; i < word.Length; i++)
+                {
+                    if (indices.Contains(i))
+                    {
+                        charCube[i] = word[i].ToString();
+                        foundAny = true;
+                    }
+                }
+
+                Console.Clear();
+
+                Design(word);
+
+                for (int i = 0; i < word.Length; i++)
+                {
+                    Console.Write($" {charCube[i]} ");
+                }
+                Console.WriteLine();
+
+                Design(word);
+
+                if (!charCube.Contains("█"))
+                {
+                    Console.Clear();
+                    Console.WriteLine($"\t\t\tПоздравляю ты отгадал слово <<{word}>> Ты ВЫИГРАЛ!!!");
+
+                    ConsoleColor originalColor = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+
+
+                    Console.WriteLine($@"
+                    
+                                                    ▄▐
+                                              ▄▄▄  ▄██▄
+                                             ▐▀█▀▌    ▀█▄
+                                             ▐█▄█▌      ▀█▄
+                                              ▀▄▀   ▄▄▄▄▄▀▀
+                                            ▄▄▄██▀▀▀▀
+                                           █▀▄▄▄█ ▀▀
+                                           ▌ ▄▄▄▐▌▀▀▀
+                                        ▄ ▐   ▄▄ █ ▀▀
+                                        ▀█▌   ▄ ▀█▀ ▀
+                                               ▄▄▐▌▄▄
+                                               ▀███▀█▄
+                                              ▐▌▀▄▀▄▀▐▄
+                                              ▐▀      ▐▌
+                                              █        █
+                                             ▐▌         █
+                                           ▄▄▌         ▄▄▌
+
+                    ");
+                    Console.ForegroundColor = originalColor;
+                    Console.ReadLine();
+                    return; // Завершаем игру
+                }
+
+                if (!foundAny)
+                {
+                    count++;
+                }
+            }
+            Console.Clear();
+            Console.WriteLine();
+            Console.WriteLine("\t\t\tК сожалению, вы проиграли. Загаданное слово было: " + word);
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine($@"
+                    
+            
+                            ██╗░░░░░░█████╗░██╗░░██╗░█████╗░██████╗░░█████╗░
+                            ██║░░░░░██╔══██╗██║░░██║██╔══██╗██╔══██╗██╔══██╗
+                            ██║░░░░░██║░░██║███████║███████║██████╔╝███████║
+                            ██║░░░░░██║░░██║██╔══██║██╔══██║██╔══██╗██╔══██║
+                            ███████╗╚█████╔╝██║░░██║██║░░██║██║░░██║██║░░██║
+                            ╚══════╝░╚════╝░╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝
+
+                    ");
+            Console.ReadLine();
+
+        }
+    }
+}
