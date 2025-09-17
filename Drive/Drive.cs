a3572a6a3f6bc9825ce21084e2509ec44b66def2
Vitaly <157066065+HaIWaeR@users.noreply.github.com>
2025-09-17 20:53:40 +0300

    Add files via upload

---
+++ Drive/Drive.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Drive
+{
+    internal class Drive
+    {
+        private int AverageСarSpeed;
+        private int CarСoordinate;
+        private double TechnicalCondition = 100;
+
+        private double pathTraveled;
+
+        public int StartСityN1 = 1010;
+        public int EndСityN1 = 1020;
+        
+        public int StartСityN2 = 3565;
+        public int EndСityN2 = 3580;
+        
+        public int StartСityN3 = -2500;
+        public int EndСityN3 = -2530;
+
+
+        public void DriveTravel(int travelTime)
+        {
+            if (TechnicalCondition > 0)
+            {
+                pathTraveled = travelTime * AverageСarSpeed;
+                CarСoordinate += travelTime * AverageСarSpeed;
+                TechnicalCondition += -0.01 * ((AverageСarSpeed * travelTime) / 100);
+                Console.WriteLine($"Вы проехали: {pathTraveled} км вперёд за {travelTime} часов");
+                Console.WriteLine($"Техническое состояние машины: {TechnicalCondition}");
+            }
+            else
+               Console.WriteLine("Машина мертва!");
+        }
+
+        public void DriveBackTravel(int travelTime)
+        {
+            if (TechnicalCondition > 0)
+            {
+                pathTraveled = travelTime * AverageСarSpeed;
+                CarСoordinate -= travelTime * AverageСarSpeed;
+                TechnicalCondition += -0.01 * ((AverageСarSpeed * travelTime) / 100);
+                Console.WriteLine($"Вы проехали: {pathTraveled} км назад за {travelTime} часов");
+                Console.WriteLine($"Техническое состояние машины: {TechnicalCondition}");
+            }
+            else
+                Console.WriteLine("Машина мертва!");
+        }
+
+        public void PrintLocation()
+        {
+            Console.WriteLine();
+            if (CarСoordinate >= StartСityN1 && CarСoordinate <= EndСityN1)
+            {
+                Console.WriteLine("Вы в городе N1");
+                return;
+            }
+
+            if (CarСoordinate >= StartСityN2 && CarСoordinate <= EndСityN2)
+            {
+                Console.WriteLine("Вы в городе N2");
+                return;
+            }
+
+            if (CarСoordinate >= StartСityN3 && CarСoordinate <= EndСityN3)
+            {
+                Console.WriteLine("Вы в городе N3");
+                return;
+            }
+            Console.WriteLine($"Вы находитесь на трассе на {CarСoordinate} километре ");
+            Console.WriteLine();
+        }
+        public Drive(int averageСarSpeed, int carСoordinate)
+        {
+            AverageСarSpeed = averageСarSpeed;
+            CarСoordinate = carСoordinate;
+        }
+    }
+}
---
+++ Drive/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Drive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Drive drive = new Drive(60, 1000);
+
+            drive.DriveTravel(6);
+            Console.WriteLine();
+
+            drive.DriveTravel(3);
+            Console.WriteLine();
+
+            drive.DriveBackTravel(3);
+            drive.PrintLocation();
+
+            drive.DriveTravel(37);
+            drive.PrintLocation();
+
+        }
+    }
+}
